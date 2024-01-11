db83e8445f6de8d10619e74fd979f659eae4eb5c
myheaven786 <139675333+myheaven786@users.noreply.github.com>
2024-01-11 22:52:48 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,72 @@
+/*
+Project Name :  🕋MyHeaven🕋
+Creator : 🕋MyHeaven🕋 
+
+*/
+
+module.exports = {
+  OWNER_NUMBER: "@🕋923066366544🕋",
+  SUDO_NUMBERS : "@🕋923066366544🕋,923066366544",
+  OWNER_NAME: "『  🕋MyHeaven🕋 』",
+  BOT_NAME : " 🕋MyHeaven786Bot🕋",
+  ZUSYCO_FILE : "ZUSYCO-QR",
+  LOGOS: {
+    ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
+    WELCOME: 'https://i.ibb.co/PzhpBV7/WELCOME-IMAGE-LOWQ.jpg',
+    GOODBYE: 'https://i.ibb.co/t8hKpcM/GOODBYE-IMAGE-LOWQ.jpg'
+  },
+  STOP_SEND : {
+        WELCOME_MESSAGE : false,
+        GOODBYE_MESSAGE : false
+    },
+  DESABLE_WELCOME_GOODBYE_JIDS : [],
+  TIME_ZONE :  "Asia/Colombo",
+  WORK_TYPE : "public",
+  WORK_MODE : "onlygroups",
+  WORK_MODE_MESSAGE_SEND : false,
+  CAPTION : '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ MyHeaven🕋 ʙᴏᴛ',
+  STICKER_PACK : '[ ZUSYCO ]',
+  BAD_WORDS : 'fuck,pussy',
+  ANTI_BAD : false,
+  ANTI_LINKS : 'chat.whatsapp.com',
+  ANTI_LINK : false,
+  ANTI_LINK_KICK : false,
+  MAX_SIZE : 100,
+  MOROCCO_NUMBERS : {
+        BLOCK : false,
+        NOT_WORKING : false,
+  },
+  READ : {
+     ALL_MESSAGES : true,
+     ONLY_COMMANDS : false,
+     STATUS : false,
+  },
+  BOT_OFFLINE : false,
+  INBOX_BLOCK : false,
+  AUTO_REACT : false,
+  OWNER_REACT : '🤵‍♂️',
+  AUTO_BGM : false,
+  BGM : {
+    "chaio" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-caio.mp3",
+    "leo" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-leo.mp3",
+    "tance" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-tance0.mp3",
+    "romant" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-floot.mp3",
+    "serio" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-serio.mp3",
+    "chill" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-wichil.mp3",
+    "yaka" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-yaka.mp3",
+    "animel" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-animel.mp3",
+    "bitch" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-bitch.mp3",
+    "papa" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-papa.mp3",
+    "shee" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-sheela.mp3",
+    "thugs" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-thugs.mp3",
+
+  },
+  AUTO_STICKER : false,
+  STICKERS : {
+    "pit" : "https://i.ibb.co/NVLMQDP/pit.webp",
+    "agei" : "https://i.ibb.co/yqk8QQ9/agei.webp",
+    "ne" : "https://i.ibb.co/N6Wb6mZ/ne.webp",
+  },
+  LOAD_ANIMATION : false,
+ 
+};
